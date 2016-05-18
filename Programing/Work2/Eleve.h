9ea641fa03a13380438208d23978a053fb9e2ebd
Dan Constantini <constantinidan@gmail.com>
2016-05-18 22:41:27 +0200

    C++ Assignments

---
+++ Programing/Work2/Eleve.h
@@ -0,0 +1,58 @@
+//
+//  Eleve.hpp
+//  untitled
+//
+//  Created by Dan Constantini on 05/11/15.
+//
+//
+# pragma once
+#ifndef Eleve_hpp
+#define Eleve_hpp
+
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+
+class Eleve{
+    string nom;
+    int note;
+public:
+    string getNom(){
+        return(nom);
+    };
+    int getNote(){
+        return(note);
+    };
+    Eleve(string name, int grade);
+    static Eleve Random();
+};
+ostream& operator<<(ostream& out, Eleve E) ;
+
+
+
+class CompareNom{
+    int* compteur;
+public:
+    CompareNom(int *c);
+    bool operator()(Eleve E1, Eleve E2); //True signifie e1<e2.
+    int getCompteur(){
+        return (*compteur);
+    }
+};
+
+
+
+class CompareNote{
+    int* compteur;
+public:
+    CompareNote(int *c);
+    bool operator()(Eleve E1, Eleve E2); //True signifie e1<e2.
+    int getCompteur(){
+        return (*compteur);
+    }
+};
+
+
+
+#endif /* Eleve_hpp */
